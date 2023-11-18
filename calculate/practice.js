606809922022dcfa832c09ad45d2b4218cd2f35d
PuchNik <i.p.chn.k@gmail.com>
2023-11-18 19:50:35 +0300

    practice

--- calculate/practice.js
+++ calculate/practice.js
@@ -1,37 +1,42 @@
-// const inputElement1 = document.getElementById('input1')
-// const inputElement2 = document.getElementById('input2')
-// const plusBtn = document.getElementById('plus')
-// const minusBtn = document.getElementById('minus')
-// let action = '+'
-// const submitBtn = document.getElementById('submit')
-// const resultElement = document.getElementById('result')
-//
-// plusBtn.onclick = function () {
-//     action = '+'
-// }
-//
-// minusBtn.onclick = function () {
-//     action = '-'
-// }
-//
-// function changeColorResult (result) {
-//     if (result < 0) {
-//         resultElement.style.color = 'red'
-//     } else if (result > 0) {
-//         resultElement.style.color = 'green'
-//     }
-//
-//     resultElement.textContent = result
-// }
-//
-// submitBtn.onclick = function () {
-//     const resultAction = resultWithAction(inputElement1, inputElement2, action)
-//     changeColorResult(resultAction)
-// }
-//
-// function resultWithAction (inp1, inp2, actionSymbol) {
-//     const num1 = Number(inp1.value)
-//     const num2 = Number(inp2.value)
-//
-//     return actionSymbol === '+' ? num1 + num2 : num1 - num2
-// }
+const checkBtn = document.getElementById('submit')
+const resultElement = document.getElementById('result')
+const inputElement_1 = document.getElementById('input1')
+const inputElement_2 = document.getElementById('input2')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
+
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+
+
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+
+function changeColor (resultColor) {
+    if (resultColor < 0) {
+        resultElement.style.color = 'red'
+    } else if (resultColor > 0) {
+        resultElement.style.color = 'green'
+    }
+
+    resultElement.textContent = resultColor
+}
+
+
+function getResult (inp1, inp2, actionSymbol) {
+    const num_1 = parseInt(inp1.value)
+    const num_2 = parseInt(inp2.value)
+
+    return actionSymbol === '+' ? num_1 + num_2 : num_1 - num_2
+}
+
+
+checkBtn.onclick = function () {
+    const result = getResult(inputElement_1, inputElement_2, action)
+    changeColor(result)
+}
