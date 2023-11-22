3c46a5a5e5d43ada193a6905a49c2e1206fa68b5
PuchNik <i.p.chn.k@gmail.com>
2023-11-22 21:26:58 +0300

    add task_2 100%

--- task_2.js
+++ task_2.js
@@ -6,9 +6,10 @@
 //     console.log('Четное')
 // } else {
 //     console.log('Нечетное')
-//     a += 1
-//     console.log('Новое значение a: ', a)
 // }
+//
+// a += 1
+// console.log('Новое значение a: ', a)
 //
 // 2)
 
@@ -20,13 +21,70 @@
 
 // 3)
 
-const array = [1, 10, 15, -100, -23, 19, 15032]
-const newArray = []
-for (let arr of array) {
-    parseFloat(arr)
-    console.log(arr)
-    arr += arr % 25
-    console.log(parseFloat(arr))
-}
+// const array = [1, 10, 15, -100, -23, 19, 15032]
+//
+// const newArray = array.map(function (num) {
+//     return num + num * 0.25
+// })
+//
+// console.log(array)
+// console.log(newArray)
 
-console.log(array + '\n')
+// let increasedArray = originalArray.map(element => element * 1.25);
+
+// 4)
+
+// function sayHello(name) {
+//     if (name) {
+//         return (`Hello, ${name}!`)
+//     } else {
+//         return (`Hello, someone!`)
+//     }
+// }
+//
+// // const sayHello = (name) => name ? `Hello, ${name}!` : `Hello, someone!`
+// console.log(sayHello())
+
+
+// 5)
+
+// function calc(a, b, operation) {
+//     if (operation === '+') {
+//         return a + b
+//     } else if (operation === '-') {
+//         return a - b
+//     } else if (operation === '*') {
+//         return a * b
+//     } else if (operation === '/') {
+//         if (b === 0) {
+//             return 'На ноль не делим)'
+//         }
+//         return a / b
+//     }
+// }
+//
+// console.log(calc(1, 0, '/'))
+
+
+// 6)
+
+// const age = 42
+// category = (age >= 18) ? 'Взрослый' : 'Детский'
+//
+// console.log('Возраст:', age);
+// console.log('Категория:', category);
+
+
+// 7)
+
+// const a = 4
+// const b = 2
+// const c = (a > b) ? a : b
+//
+// console.log(c)
+
+
+
+
+
+
