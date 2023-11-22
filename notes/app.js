80857470db781df4c72cc9b6708b8a3a80afd6b6
PuchNik <i.p.chn.k@gmail.com>
2023-11-22 19:53:52 +0300

    repeat practice 4

--- notes/app.js
+++ notes/app.js
@@ -30,3 +30,68 @@
 //
 // arrow3('Nik_3')
 //
+// 4) Метод .with - Создание нового (измененного) массива
+// const names = ['Nik', 'Ann', 'Nat', 'Pash']
+// const greateWoman = 'Ann'
+// const index = names.indexOf(greateWoman)
+// console.log(index)
+// const newNames = names.with(index, 'Ann greate')
+// console.log(names)
+// console.log(newNames)
+
+// 5) Метод .map - создаёт новый массив с результатом вызова указанной функции для каждого элемента массива.
+// const names = ['Nik', 'Ann', 'Nat', 'Pash']
+// const capitalNames = names.map(function (name, index) {
+//     if (index === 1) {
+//         return 'Ann Greate'
+//     }
+//     return name
+// })
+// console.log(names)
+// console.log(capitalNames)
+
+// 6)
+
+// const people = [
+//     { name: 'Владилен', budget: 4200 },
+//     { name: 'Елена', budget: 15100 },
+//     { name: 'Игорь', budget: 300 },
+//     {name: 'Ксения', budget: 7520 },
+// ]
+
+// const finded = people.find(function (person) {
+//     return person.budget === 7520
+// })
+
+// const finded = people.find((p) => p.budget === 7520)
+// console.log(finded)
+
+// 7) Список часто используемых функций:
+
+const list = [
+    {
+        title: 'map()',
+        id: 1,
+    },
+
+    {
+        title: 'find()',
+        id: 2,
+    },
+
+    {
+        title: 'findIndex()',
+        id: 3,
+    },
+
+    {
+        title: 'filter()',
+        id: 4,
+    },
+
+    {
+        title: 'forEach()',
+        id: 5,
+    },
+]
+
